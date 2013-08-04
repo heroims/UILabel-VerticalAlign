83cecedfcb9290fe63c672a0a7290a1f04ca0fb6
ZhaoYiqi <heroims@163.com>
2013-08-04 16:27:07 +0800

    上传

---
+++ UILabel+VerticalAlign/UILabel+VerticalAlign.h
@@ -0,0 +1,16 @@
+//
+//  UILabel+VerticalAlign.h
+//  xiguateng
+//
+//  Created by Zhao Yiqi on 12-10-26.
+//  Copyright (c) 2012年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (VerticalAlign)
+
+- (void)alignTop;
+- (void)alignBottom;
+
+@end
